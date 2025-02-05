9247331f8ddf37b0310b44ef99dfd59666e11f84
Ian Chin <63411195+jive46@users.noreply.github.com>
2025-02-05 10:29:22 +0800

    Add PWA features

--- pages/_document.js
+++ pages/_document.js
@@ -6,6 +6,15 @@ export default function Document() {
       <Head>
         <meta name="robots" content="noindex, nofollow" />
         <link rel="icon" href="/favicon.ico" />
+        {/* Link to the manifest */}
+        <link rel="manifest" href="/manifest.json" />
+        {/* Set theme color */}
+        <meta name="theme-color" content="#2d3a55" />
+        {/* Apple-specific tags for iOS support */}
+        <link rel="apple-touch-icon" href="/icons/glovidalogosq192x192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        {/* You can add other meta tags for splash screen images if needed */}
       </Head>
       <body>
         <Main />
